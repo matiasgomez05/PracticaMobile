475c2f0194a7829c2a2989faeb1a61dc7f6b4f4b
Matias Gomez <matias_gomez05@hotmail.com>
2022-10-19 15:34:40 -0300

    Test Api y manejo de imagen

--- App.js
+++ App.js
@@ -11,12 +11,13 @@ export default function App() {
   const [imagen, setImagen] = React.useState("");
 
   function obtenerJuegoRandom(){
-    let random = Math.floor(Math.random() * 500); 
-    const endpoint = "https://www.freetogame.com/api/game?id="+random;
+    let juegoRandom = Math.floor(Math.random() * 367); 
+    const endpoint = "https://www.freetogame.com/api/game?id="+juegoRandom;
+
     axios.get(`${endpoint}`)
-      .then( (respuesta) => {
+    .then( (respuesta) => {
         let juego = respuesta.data.title;
-        let imagen = respuesta.data.thumbnail;
+        let imagen = { uri: respuesta.data.thumbnail }
 
         setJuego(juego);
         setImagen(imagen);
@@ -28,7 +29,7 @@ export default function App() {
 
   return (
     <View style={styles.container}>
-      <Image source={ imagen } />
+      <Image source={ imagen } onChange={ (e) => setImagen(e.target.value) } style={styles.imagen}/>
       <Text style={styles.text1}>Juegos gratis al azar:</Text>
       <Button onPress={ () => obtenerJuegoRandom() } title="Descubrir"></Button>
       <Text style={styles.text2}>{ juego }</Text>
@@ -39,18 +40,29 @@ export default function App() {
 
 const styles = StyleSheet.create({
   container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
+    display: "flex",
+    justifyContent: "center",
+    alignItems: "center",
+    backgroundColor: '#000000',
+    height: "100%",
+    width: "100%"
   },
   text1: {
-    margin: 10,
+    margin: 5,
     fontWeight: "bold",
+    color: "white"
   },
   text2: {
     margin: 10,
     fontWeight: "bold",
-    color: "red",
+    color: "yellow",
+  },
+  imagen: {
+    resizeMode: "cover",
+    height: 206,
+    width: 365,
+    margin: 5,
+    borderWidth: 3,
+    borderColor: "yellow",
   }
 });
